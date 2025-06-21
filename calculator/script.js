a0b294e1cedbedf103292f1c392044c8f0e6b314
priyanshugitt <priyanshujain702@gmail.com>
2025-06-21 11:24:19 +0530

    commited

---
+++ calculator/script.js
@@ -0,0 +1,8 @@
+function addition(){
+    let a= document.getElementById("num_1").value;
+    let b= document.getElementById("num_2").value;
+
+   let c= parseInt(a)+ parseInt(b);
+    console.log(c)
+    //   document.getElementById("result").innerHTML=c;
+}
